8bd9b78848710b5c1862cbf50ae71235fadb497c
Jesus-1996 <38133794+koussaykhelil@users.noreply.github.com>
2018-06-25 11:56:13 +0100

    Add project

---
+++ your first function/Assets/maths.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maths : MonoBehaviour
+{
+    public float a;
+    public float b;
+
+    // Use this for initialization
+    void sum()
+    {
+        Debug.Log(a + b);
+    }
+    void substract()
+    {
+        Debug.Log(a - b);
+    }
+    void multiplication()
+    {
+        Debug.Log(a * b);
+    }
+    void Start()
+    {
+        sum();
+        substract();
+        multiplication();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
